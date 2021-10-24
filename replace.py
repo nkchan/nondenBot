6c08560642f276c192b61803625c2283a3ce0ab4
Kaoru Nagashima <nkchan0228@gmail.com>
2021-10-25 00:28:48 +0900

    ini

--- replace.py
+++ replace.py
@@ -6,8 +6,8 @@ with open(file_name, encoding="cp932") as f:
     data_lines = f.read()
 
 # 文字列置換
-data_lines = data_lines.replace("LINE_CHANNEL_ACCESS_TOKEN",os.environ["CHANNEL_ACCESS_TOKEN"])
-data_lines = data_lines.replace("LINE_GROUPID",os.environ["GROUPID"])
+data_lines = data_lines.replace("LINE_CHANNEL_ACCESS_TOKEN",os.environ.get("CHANNEL_ACCESS_TOKEN"))
+data_lines = data_lines.replace("LINE_GROUPID",os.environ.get("GROUPID"))
 
 # 同じファイル名で保存
 with open(file_name, mode="w", encoding="cp932") as f:
